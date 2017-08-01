92ee59355d8710b7e2237588e78b497695e4b557
parksdrew01@gmail.com and parul.mishra78@gmail.com <Guest@epicodus-39.local>
2017-08-01 10:41:27 -0700

    this commit will add a todolist

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+
+namespace ToDoList.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        [Route("/")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("/task/list")]
+        public ActionResult TaskList()
+        {
+          List<string> allTasks = Task.GetAll();
+          return View(allTasks);
+        }
+
+        [HttpPost("/task/create")]
+        public ActionResult CreateTask()
+        {
+          Task newTask = new Task(Request.Form["new-task"]);
+          newTask.Save();
+          return View(newTask);
+        }
+        [HttpPost("/task/list/clear")]
+         public ActionResult TaskListClear()
+         {
+             Task.ClearAll();
+             return View();
+         }
+
+    }
+}
